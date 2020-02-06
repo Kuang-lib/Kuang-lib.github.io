aeafe6fbff1a566e6748f4e4b0a338fb18a9d697
Kuang-lib <1638907037@qq.com>
2020-02-06 16:47:45 +0800

    Site updated: 2020-02-06 16:47:34

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,20 +1,19 @@
 /* 离开当前页面时修改网页标题，回到当前页面时恢复原来标题 */
 window.onload = function() {
-  var OriginTitile = document.title;
-  var titleTime;
-  document.addEventListener('visibilitychange', function() {
-    if(document.hidden) {
-      $('[rel="icon"]').attr('href', "../../images/failure.png");
-      $('[rel="shortcut icon"]').attr('href', "../../images/failure.png");
-      document.title = '(●—●)喔哟，崩溃啦！';
-      clearTimeout(titleTime);
-    } else {
-	  $('[rel="icon"]').attr('href', "../../images/favicon-32x32.ico.png");
-	  $('[rel="shortcut icon"]').attr('href', "../../images/favicon-32x32.ico.png");
-	  document.title = 'o(^▽^)o 被你发现啦~ ';
-	  titleTime = setTimeout(function () {
-		document.title = OriginTitile;
-	  }, 2000);
-    }
-  });
+ var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/img/failure.png");
+         document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/favicon-32x32.ico.png");
+         document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitle;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });
 };
